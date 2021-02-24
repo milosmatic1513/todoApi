01bd358828711e890cb0b83d591e2fe7d89b9057
milosmatic1513 <milos.matic1513@gmail.com>
2021-02-24 13:59:15 +0200

    Fixed Urls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   include Response
   include ExceptionHandler
-
+  include CurrentUserConcern
   skip_before_action :verify_authenticity_token
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
 class SessionsController < ApplicationController
+
   before_action :set_current_user
+
   def create
     user = User
             .find_by(email: params["email"])
@@ -30,16 +32,34 @@ class SessionsController < ApplicationController
     end
   end
 
+  def login
+    user = User
+            .find_by(email: params["email"])
+            .try(:authenticate, params["password"])
+
+    if user
+      session[:user_id] = user.id
+      render json: {
+        status: :created,
+        logged_in: true,
+        user: user
+      }
+    else
+      render json: { status: 401 }
+    end
+  end
+
+  def logout
+    reset_session
+    render json: { status: 200 }
+  end
+
   private
 
   def user_params
     params.permit(:email,:password)
   end
 
-  def set_current_user
-    if session[:user_id]
-      @current_user = User.find(session[:user_id])
-    end
-  end
+
 
 end
