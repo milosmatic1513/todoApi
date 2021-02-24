099e555bc3dea6467599fbd52dd18c1411e1e9fe
milosmatic1513 <milos.matic1513@gmail.com>
2021-02-24 18:50:49 +0200

    Added heroku path to variables

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,4 +10,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
    origins "http://localhost:3000"
    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
  end
+ allow do
+  origins "http://rails-todo-api-app.herokuapp.com"
+  resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+end
+
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_todo_app"
+if Rails.env =="production"
+  Rails.application.config.session_store :cookie_store, key: "_todo_app" ,domain:"rails-todo-api-app.herokuapp.com"
+else
+  Rails.application.config.session_store :cookie_store, key: "_todo_app" 
+end
